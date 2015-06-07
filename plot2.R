9501fa9769a6e33252c836d815037a52482d69c8
Reza M <zanboor13@gmail.com>
2015-06-07 17:48:14 -0400

    plot 2

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-plot1 <- function(){
+plot2 <- function(){
       
       ## Read the data file
       data <- read.table("household_power_consumption.txt",
@@ -12,11 +12,14 @@ plot1 <- function(){
       ## just grab rows for the two days we are interested in
       data <-data[data$Date == "2007-02-01" | data$Date =="2007-02-02",]
       
-      ## Create a histogram
-      hist(data2$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+      ## Create a new column that has both date and time in it
+      data$DateTime <- with(data, as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M"))
+      
+      ## Create a linear plot
+      plot(data$Global_active_power~data$DateTime, xlab="",ylab="Global Active Power (kilowatts)", type="l")
       
       ##  Copy to a local PNG file
-      dev.copy (png, file="./plot1.png")
+      dev.copy (png, file="./plot2.png")
       
       ## Close the device (complete the file creation)
       dev.off()
